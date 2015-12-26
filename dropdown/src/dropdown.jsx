549560973aa133aac4040678915ba5a9839e2b1a
KyleRattet <krattet@gmail.com>
2015-12-25 21:18:25 -0700

    dropdowm

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,7 +4,7 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function (){
-    alert('hello');
+    alert('im a dropdown');
   },
   render: function () {
     var list = this.props.items.map(function(item){
